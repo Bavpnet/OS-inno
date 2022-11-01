8d02ae8e1935540014e1192517e4f95cfe4cbd39
Alexandra Chupkova <88327718+Bavpnet@users.noreply.github.com>
2022-11-01 20:56:14 +0300

    Update ex3.c

--- week08/ex3.c
+++ week08/ex3.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
  struct rusage usage;
  for (int i = 0; i < 10; i++) {
     p[i] = malloc(10*1024*1024);   
-    memset(p[i], 0, 10 * 1024 * 1024);
+    memset(p[i], 0, 10 * 1024 * 1024); //here I set 10MB of memory, and I got 1.0% CPU
+    // and 1.3% of MEM on Linux Vitrual Machine
     sleep(1);
  }
  for (int i = 0; i < 10; i++){
